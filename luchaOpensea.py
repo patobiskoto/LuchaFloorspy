e6ed5a1c691fd8bce0b551238774632466750d70
Baptiste PREALPATO <bpdiv@pm.me>
2021-12-30 19:04:51 +0100

    debug heroku

--- luchaOpensea.py
+++ luchaOpensea.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 
 class OpenseaQuerries:
     async def findAFloor(nbAttr):
-        headers = {'X-Api-Key':config['opensea_api_key'],'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0'}
+        headers = {'X-Api-Key':str(config['opensea_api_key']),'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0'}
         opensea_url = "https://opensea.io/collection/luchadores-io?search[numericTraits][0][name]=Attributes&search[numericTraits][0][ranges][0][max]="+nbAttr+"&search[numericTraits][0][ranges][0][min]="+nbAttr+"&search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW"
+        print(opensea_url)
         resp = requests.get(url=opensea_url,headers=headers)
         
         bswebpage = BeautifulSoup(resp.text, "html.parser")
