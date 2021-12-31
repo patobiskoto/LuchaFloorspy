0636e74d253c1fba3bfcbe7c3e5328c2f9fe9e79
Baptiste PREALPATO <bpdiv@pm.me>
2021-12-31 12:55:03 +0100

    add debug options

--- luchaOpensea.py
+++ luchaOpensea.py
@@ -8,6 +8,9 @@ class OpenseaQuerries:
         opensea_url = "https://opensea.io/collection/luchadores-io?search[numericTraits][0][name]=Attributes&search[numericTraits][0][ranges][0][max]="+nbAttr+"&search[numericTraits][0][ranges][0][min]="+nbAttr+"&search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW"
 
         resp = scraper.get(url=opensea_url)
+        if resp.status_code != 200:
+            print("--- status code = " + resp.status_code)
+            print(resp.text)
         bswebpage = BeautifulSoup(resp.content, "html.parser")
 
         for assetCardFooter in bswebpage.findAll('div',{'class':'AssetCardFooter--price'}):
