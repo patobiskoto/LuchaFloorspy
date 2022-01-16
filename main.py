26fae1776a4bdf60c799de2a51408fa4258e525d
Baptiste PREALPATO <bpdiv@pm.me>
2022-01-16 11:48:00 +0100

    update eth + new test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from datetime import datetime
-from config import config
-from discord.ext.commands.bot import Bot
-from luchaOpensea import OpenseaQuerries
-from discord.ext import commands, tasks
+
 from discord import Embed
+from discord.ext import commands, tasks
+from discord.ext.commands.bot import Bot
+
+from config import config
+from luchaOpensea import OpenseaQuerries
 
 luchaFloors = commands.Bot(command_prefix = "!", description = "LuchaFloors")
 
@@ -37,7 +39,7 @@ class Processors:
         i = 0
         while i < 8:
             print("-- request for " + str(i) + "T")
-            embed.add_field(name=str(i) + "T", value=str(await OpenseaQuerries.findAFloor(str(i))) + " :eth:", inline=False)
+            embed.add_field(name=str(i) + "T", value=str(await OpenseaQuerries.findAFloor(str(i))) + "Ξ", inline=False)
             i = i + 1
         embed.set_footer(text="luchadores.io", icon_url=config["lucha_icon_url"])
         return embed
